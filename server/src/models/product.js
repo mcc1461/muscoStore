8d6f01bb178f0d282e83b783f954f6cddbdc2c54
mcc1461 <mcoskuncelebi@gmail.com>
2024-09-06 09:14:12 +0300

    feat:muscoStoreProducts json file arranged

--- server/src/models/product.js
+++ server/src/models/product.js
@@ -19,22 +19,36 @@ const ProductSchema = new mongoose.Schema(
       ref: "Category",
       required: true,
     },
-
     brandId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Brand",
       required: true,
     },
-
     name: {
       type: String,
       trim: true,
       required: true,
     },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
 
     quantity: {
       type: Number,
       default: 0,
+    },
+    image: {
+      type: String,
+      default: "no-image.jpg",
+    },
+    image2: {
+      type: String,
+      default: "no-image.jpg",
     },
   },
   { collection: "products", timestamps: true }
